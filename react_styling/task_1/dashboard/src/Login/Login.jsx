d37d225982c38203cf02ecad31dd89a6c1da0cb4
nimrod02 <letrun.a@gmail.com>
2025-11-26 16:29:58 +0100

    test

---
+++ react_styling/task_1/dashboard/src/Login/Login.jsx
@@ -0,0 +1,17 @@
+import WithLogging from '../HOC/WithLogging';
+import './Login.css';
+
+export default WithLogging(() => {
+  return (
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+      <div className="form">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="user_email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="user_password" id="password" />
+        <button>OK</button>
+      </div>
+    </div>
+  )
+});
